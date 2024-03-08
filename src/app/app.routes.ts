1f874469000b1cdb57a350620783c30cde32109f
Juan Manuel Herrera Carrera <tampiko@gmail.com>
2024-03-08 15:47:06 -0600

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,7 +14,6 @@ export const routes: Routes = [
     path: 'checkout',
     loadComponent: () => import('./features/checkout/checkout.component'),
   },
-
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: '**', redirectTo: 'products', pathMatch: 'full' },
 ];
